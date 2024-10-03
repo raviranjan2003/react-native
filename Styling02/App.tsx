d6c07f645c6aa29d691257e053d0f2a931fc87f7
Ravi <2140131@sliet.ac.in>
2024-10-04 01:33:45 +0530

    styling app 02
    flex properties

---
+++ Styling02/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { 
+  SafeAreaView,
+  ScrollView,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>App</Text>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App;
